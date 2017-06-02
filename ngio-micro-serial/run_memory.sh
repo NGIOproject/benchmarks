eed503d2c8abd4074dd129007398fa6ef93f0cbf
Adrian Jackson <a.jackson@epcc.ed.ac.uk>
2017-06-02 17:10:07 +0100

    Including all the vmem tests in the example run script

--- ngio-micro-serial/run_memory.sh
+++ ngio-micro-serial/run_memory.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 size=1000000
-
+nvme_location=/media/nvme1
 
  ./micro -b memory -s ${size} -o calloc
- ./micro -b memory -s ${size} -o pvmem_calloc -l /media/nvme1
+ ./micro -b memory -s ${size} -o pvmem_calloc -l ${nvme_location}
  ./micro -b memory -s ${size} -o read_ram
- ./micro -b memory -s ${size} -o read_vram -l /media/nvme1
+ ./micro -b memory -s ${size} -o read_vram -l ${nvme_location}
  ./micro -b memory -s ${size} -o read_lat
- ./micro -b memory -s ${size} -o vmem_read_lat -l /media/nvme1
+ ./micro -b memory -s ${size} -o vmem_read_lat -l ${nvme_location}
  ./micro -b memory -s ${size} -o write_contig
- ./micro -b memory -s ${size} -o write_vmem_contig -l /media/nvme1
+ ./micro -b memory -s ${size} -o write_vmem_contig -l ${nvme_location}
  ./micro -b memory -s ${size} -o write_strided
+ ./micro -b memory -s ${size} -o write_vmem_strided -l ${nvme_location}
  ./micro -b memory -s ${size} -o write_random
+ ./micro -b memory -s ${size} -o write_vmem_random -l ${nvme_location}
